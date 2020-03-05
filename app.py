9fc1879a6066e75772e30e6cc06435a3a27edc94
raffraye <elouan.raffray@etu.esisar.grenoble-inp.fr>
2020-03-05 13:00:08 +0100

    added reqs and row/col randomizing

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from imageclassifier import ImageClassifier
 from flask import Flask, render_template, request
 import logging
 import time
+import cv2
+import random
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 app = Flask(__name__)
+
+
+old_target = None
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
@@ -14,19 +19,44 @@ def root():
         return render_template('index.html')
     elif request.method == 'POST':
         nb_clusters = request.form['clusters']
+        if nb_clusters == '':
+            return render_template('index.html')
         img = request.files['img']
-        ext = img.mimetype.split('/')[1]
-        path = "static/target." + ext
-        img.save(path)
+        if img.filename != '':
+            in_name = old_target
+        else:    
+            ext = img.mimetype.split('/')[1]
+            in_name = "target_ " + str(time.time()) + "." + ext
+            img.save('static/' + in_name)
+            old_target = in_name
+        
         dst_name = "res_" + str(time.time()) + ".png"
 
-
         if request.form['effect'] == 'sort':
-            classifier = ImageClassifier(int(nb_clusters), path)
+            classifier = ImageClassifier(int(nb_clusters), 'static/' + in_name)
             classifier.run('static/' + dst_name)
+        elif request.form['effect'] == 'rand_row':
+            randomize('static/' + in_name, 'static/' + dst_name)
+        elif request.form['effect'] == 'rand_col':
+            randomize('static/' + in_name, 'static/' + dst_name, cols=True)
         
+        return render_template('index.html', path_in=in_name, path_out=dst_name)
 
+def randomize(path_in, path_out, cols=False):
+    img = cv2.imread(path_in)
 
+    if cols:
+        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-        return render_template('index.html', img=dst_name)
-        
+    rows,_,_ = img.shape
+    out = img.copy()
+    new_idx = [i for i in range(rows)]
+    random.shuffle(new_idx)
+
+    for i in range(rows):
+        out[i] = img[new_idx[i]]
+    
+    if cols:
+        out = cv2.rotate(out, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    cv2.imwrite(path_out, out)
